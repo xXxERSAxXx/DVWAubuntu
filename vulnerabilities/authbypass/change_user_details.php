c56716c63e20650d05d1938e8109215e8cec60da
ElkinMG <elkinmoscoso1204346@correo.itm.edu.co>
2026-06-11 01:10:20 +0000

    correcion de Vulnerabilidad SQLI CWE89 en change_user_details

--- vulnerabilities/authbypass/change_user_details.php
+++ vulnerabilities/authbypass/change_user_details.php
@@ -44,8 +44,11 @@ try {
 	exit;
 }
 
-$query = "UPDATE users SET first_name = '" . $data->first_name . "', last_name = '" .  $data->surname . "' where user_id = " . $data->id . "";
-$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+$query = "UPDATE users SET first_name = ?, last_name = ? WHERE user_id = ?";
+$stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], $query);
+mysqli_stmt_bind_param($stmt, "ssi", $data->first_name, $data->surname, $data->id);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 
 print json_encode (array ("result" => "ok"));
 exit;
