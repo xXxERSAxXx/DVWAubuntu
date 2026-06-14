ef13b32f2b53deb4fa6a65302e880f382a92bd80
Erik Gil <santiagogil12@hotmail.com>
2026-06-14 19:02:22 -0400

    Correcion: Mitigacion de SQLi, XSS y File Upload en DVWA

--- vulnerabilities/upload/source/low.php
+++ vulnerabilities/upload/source/low.php
@@ -1,19 +1,18 @@
 <?php
-
 if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
-	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
+    $target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/" . basename( $_FILES[ 'uploaded' ][ 'name' ] );
 
-	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-		// No
-		$html .= '<pre>Your image was not uploaded.</pre>';
-	}
-	else {
-		// Yes!
-		$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
-	}
+    // Parche: Validar tipo MIME y extensión según guía
+    $uploaded_type = $_FILES[ 'uploaded' ][ 'type' ];
+    $uploaded_ext  = strtolower(pathinfo($_FILES[ 'uploaded' ][ 'name' ], PATHINFO_EXTENSION));
+
+    if( ($uploaded_ext == "jpeg" || $uploaded_ext == "png") && 
+        ($uploaded_type == "image/jpeg" || $uploaded_type == "image/png") ) {
+        move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path );
+        echo "<pre>Successfully uploaded!</pre>";
+    } else {
+        echo '<pre>Invalid file type.</pre>';
+    }
 }
-
 ?>
+
--- vulnerabilities/xss_r/source/low.php
+++ vulnerabilities/xss_r/source/low.php
@@ -1,11 +1,9 @@
 <?php
-
 header ("X-XSS-Protection: 0");
-
-// Is there any input?
 if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
-	// Feedback for end user
-	$html .= '<pre>Hello ' . $_GET[ 'name' ] . '</pre>';
+    
+    // Parche: Escapar entradas con htmlspecialchars según guía
+    $name = htmlspecialchars($_GET[ 'name' ], ENT_QUOTES, 'UTF-8');
+    echo '<pre>Hello ' . $name . '</pre>';
 }
-
 ?>
